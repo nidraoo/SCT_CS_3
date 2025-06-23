6ecbe114859d1f68611210d70dfe4656395a1f15
Nidhi Rao <05nidzzzzz@gmail.com>
2025-06-23 22:33:42 +0530

    Update password_check.py
    added more buttons for copy and clear and made the gui more better, final edit

--- password_checker/password_check.py
+++ password_checker/password_check.py
@@ -1,5 +1,4 @@
 import tkinter as tk #in built module for GUI
-from tkinter import ttk #tkk themed tkinter widgets, use Entry, Button , label with better styling,platform native lookinh
 import string #useful constants and functions for handling strings, alphabets, numbers, punctuations
 import re #built in regular expression engine, It is used to check patterns in string 
 import secrets #it is designed for generating cryptographically secure random numbers. it is better than random for generating passwords,tokens and anything security related
@@ -60,9 +59,9 @@ def evaluate_password(password):
         color='green'
     else:
         strength='VERY STRONG!!!'
-        color='dark green'
+        color='black'
         
-    return strength, score, suggestions,color
+    return strength,suggestions,color
 
 #pwned passwords API chechker
 def check_pwned_password(password):
@@ -86,7 +85,7 @@ def on_key_release(event=None): #this is for ui callback
         strength_label.config(text='Strength: ')
         suggestions_text.delete(0,tk.END)
         return 
-    strength,score,suggestions,color=evaluate_password(password)
+    strength,suggestions,color=evaluate_password(password)
     pwned_count = check_pwned_password(password) 
     
     if isinstance(pwned_count,int):
@@ -97,20 +96,37 @@ def on_key_release(event=None): #this is for ui callback
     else:
         breach_info = pwned_count
         
-    strength_label.config(text=f'Strength: {strength} (Scroe: {score}/7\n{breach_info}',fg=color)  
+    strength_label.config(text=f'Strength: {strength}\n{breach_info}',fg=color)  
     suggestions_text.delete(0,tk.END)
     for sug in suggestions:
         suggestions_text.insert(tk.END,"• " + sug)
     
 #making the show or hide toggle for the password
 def toggle_password():
-    if password_entry.cget('show')=='*':
+    global show_password
+    show_password = not show_password
+    password = password_entry.get()
+    if show_password:
         password_entry.config(show='')
         toggle_btn.config(text='Hide')
     else:
-        password_entry.config(show='*') 
+        password_entry.config(show="• ") 
         toggle_btn.config(text="Show")
         
+def copy_to_clipboard():
+    password = password_entry.get()
+    window.clipboard_clear()
+    window.clipboard_append(password)
+    window.update()
+    copied_label.config(text="Copied ✅")
+    copied_label.after(2000,lambda: copied_label.config(text=''))
+
+def clear_fields():
+    password_entry.delete(0, tk.END)
+    strength_label.config(text='Strength: ', fg='black')
+    suggestions_text.delete(0, tk.END)
+    copied_label.config(text='')
+    
 #password generating
 def generate_password():
     length =int(length_slider.get())
@@ -122,42 +138,68 @@ def generate_password():
 
 window =tk.Tk()
 window.title("🔐 Password Strength Evaluator + Generator")
-window.geometry("550x480")
+window.geometry("600x600")
+window.configure(bg='#f2f2f2')
 window.resizable(False, False)
+show_password= False
 
-style =ttk.Style()
-style.configure("TLabel",font=("Segoe UI",10))
-style.configure("TEntry",font=("Segoe UI",10))
+title_label= tk.Label(window,text='Password Evaluator & Generator',font=('Times New Roman',18,'bold italic'),bg='#f2f2f2',fg='#333')
+title_label.pack(pady=20)
 
-entry_frame = ttk.Frame(window)
+entry_frame = tk.Frame(window,bg='#f2f2f2')
 entry_frame.pack(pady=15)
 
-ttk.Label(entry_frame, text='Enter Password: ',font=("Segoe UI",12,'bold')).grid(row=0,column=0,padx=5)
-password_entry=ttk.Entry(entry_frame, width=35,show='*')
+tk.Label(entry_frame, text='Enter Password: ',font=("Segoe UI",12,'bold'),bg='#f2f2f2').grid(row=0,column=0,padx=5)
+password_entry=tk.Entry(entry_frame, width=35,show='•',font=('Segoe Ui',11))
 password_entry.grid(row=0,column=1)
 password_entry.bind("<KeyRelease>",on_key_release)
 
-toggle_btn = ttk.Button(entry_frame, text='Show',command=toggle_password, width=6)
-toggle_btn.grid(row=0,column=2,padx=5)
+btn_frame = tk.Frame(window, bg='#f2f2f2')
+btn_frame.pack(pady=5)
 
-strength_label= tk.Label(window,text='Strength: ',font=('Seogoe UI',11,'bold'))
+toggle_btn = tk.Button(btn_frame, text='Show', command=toggle_password, width=8, font=('Segoe UI', 9), bg='#4da6ff', fg='white', activebackground='#007acc')
+toggle_btn.pack(side=tk.LEFT, padx=5)
+
+copy_btn = tk.Button(btn_frame, text='Copy', command=copy_to_clipboard, width=8, font=('Segoe UI', 9), bg='#ffaa00', fg='white', activebackground='#cc8400')
+copy_btn.pack(side=tk.LEFT, padx=5)
+
+clear_btn = tk.Button(btn_frame, text='Clear', command=clear_fields, width=8, font=('Segoe UI', 9), bg='#ff4d4d', fg='white', activebackground='#cc0000')
+clear_btn.pack(side=tk.LEFT, padx=5)
+
+copied_label = tk.Label(window,text='',font=('Segoe UI',9),bg='#f2f2f2',fg="green")
+copied_label.pack()
+
+strength_label= tk.Label(window,text='Strength: ',font=('Seogoe UI',12,'bold'))
 strength_label.pack(pady=10)
 
-ttk.Label(window,text="Suggestions to Improve: ",font=('Segoe UI',11)).pack()
+tk.Label(window,text="Suggestions to Improve: ",font=('Segoe UI',12,'bold')).pack()
 suggestions_text= tk.Listbox(window,width=60,height=6)
 suggestions_text.pack(pady=5)
 
-generator_frame=ttk.Frame(window)
+generator_frame=tk.Frame(window,bg='#f2f2f2')
 generator_frame.pack(pady=10)
 
-ttk.Label(generator_frame,text='Generate Strong Password: ',font=('Segoe UI',11,'bold')).grid(row=0,columnspan=2,pady=5)
-ttk.Label(generator_frame,text='Length: ').grid(row=1,column=0,padx=5)
+tk.Label(generator_frame,text='Generate Strong Password: ',font=('Segoe UI',11,'bold'),bg='#f2f2f2').grid(row=0,columnspan=2,pady=5)
+tk.Label(generator_frame,text='Length: ').grid(row=1,column=0,padx=5)
 
-length_slider = ttk.Scale(generator_frame,from_=8,to=32,orient='horizontal',length=200)
+#length_slider = tk.Scale(generator_frame,from_=8,to=32,orient='horizontal',length=200,bg='#f2f2f2')
+length_slider = tk.Scale(
+    generator_frame,
+    from_=8,
+    to=32,
+    orient='horizontal',
+    length=200,
+    bg='#f2f2f2',              # background of widget
+    fg='black',                # text color (not always visible on sliders)
+    troughcolor='#d3d3d3',     # the bar's track color
+    activebackground="#4CAF50",# color when actively sliding
+    sliderrelief='raised',     # visual depth of the knob
+    font=('Segoe UI', 10)
+)
 length_slider.set(12)
 length_slider.grid(row=1,column=1)
 
-generate_btn=ttk.Button(generator_frame,text='Generate Password',command=generate_password)
+generate_btn=tk.Button(generator_frame,text='Generate Password',command=generate_password,font=('Segoe UI',10),bg='#4CAF50', fg='white', activebackground='#388e3c')
 generate_btn.grid(row=2,column=0,columnspan=2,pady=10)
 
 window.mainloop()
